9e4f08a1c7567f3d0fbd322f483dad3db772d2d4
Rakandiya <rakandiya.safwan@gmail.com>
2024-06-10 21:59:46 +0700

    Add TBD Feature

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -12,26 +12,27 @@ class HomeController extends Controller
     //
 
     public function index() {
-        // $results = DB::select("
-        //     SELECT 
-        //         ranking_query.user_id,
-        //         users.name,
-        //         users.email,
-        //         ranking_query.total_booking,
-        //         DENSE_RANK() OVER (ORDER BY ranking_query.total_booking DESC) AS rank
-        //     FROM (
-        //         SELECT 
-        //             user_id,
-        //             COUNT(*) AS total_booking
-        //         FROM 
-        //             bookings
-        //         GROUP BY 
-        //             user_id
-        //     ) AS ranking_query
-        //     JOIN users ON users.id = ranking_query.user_id
-        //     ORDER BY ranking_query.total_booking DESC
-        //     LIMIT 5
-        // ");
+        $results = DB::select("
+            SELECT 
+                ranking_query.user_id,
+                users.nama,
+                users.foto,
+                ranking_query.total_booking,
+                DENSE_RANK() OVER (ORDER BY ranking_query.total_booking DESC) AS rank
+            FROM (
+                SELECT 
+                    user_id,
+                    COUNT(*) AS total_booking
+                FROM 
+                    bookings
+                GROUP BY 
+                    user_id
+            ) AS ranking_query
+            JOIN users ON users.id = ranking_query.user_id
+            ORDER BY ranking_query.total_booking DESC
+            LIMIT 5
+        ");
+
         return Inertia::render('User/Home', compact('results'));
     }
 }
--- resources/js/Pages/User/Home.jsx
+++ resources/js/Pages/User/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import UserLayout from "@/Layouts/UserLayout";
 import styles from "../../../css/User/Home.module.css";
 
-const HeroSection = ({ auth }) => {
+const HeroSection = ({ auth, results }) => {
     console.log(auth);
     return (
         <UserLayout auth={auth}>
@@ -44,9 +44,13 @@ const HeroSection = ({ auth }) => {
                 </div>
             </section>
 
-            <section className="section work" aria-labelledby="work-label">
+            <section
+                className="section work"
+                style={{ padding: "0 0 50px" }}
+                aria-labelledby="work-label"
+            >
                 <div className={`${styles["container"]}`}>
-                    <p
+                    {/* <p
                         className={`${styles["section-subtitle"]} ${styles["light"]}`}
                         style={{
                             color: "white",
@@ -54,7 +58,7 @@ const HeroSection = ({ auth }) => {
                         id="work-label"
                     >
                         TOTAL
-                    </p>
+                    </p> */}
 
                     <h2
                         className={`${styles["h2"]} ${styles["section-title"]}`}
@@ -62,58 +66,64 @@ const HeroSection = ({ auth }) => {
                             color: "white",
                         }}
                     >
-                        BOOKING
+                        Customer Teratas
                     </h2>
 
                     <ul className={`${styles["has-scrollbar"]}`}>
-                        <li className={`${styles["scrollbar-item"]}`}>
-                            <div className={`${styles["work-card"]}`}>
-                                <figure
-                                    className={`${styles["card-banner"]} ${styles["img-holder"]}`}
-                                    style={{
-                                        width: "350px",
-                                        height: "406px",
-                                    }}
-                                >
-                                    <img
-                                        src="/images/work-1.png"
-                                        width="350"
-                                        height="406"
-                                        loading="lazy"
-                                        alt="Engine Repair"
-                                        className={`${styles["img-cover"]}`}
-                                    />
-                                </figure>
-
-                                <div className={`${styles["card-content"]}`}>
-                                    <p className={`${styles["card-subtitle"]}`}>
-                                        PERINGKAT 1
-                                    </p>
-
-                                    <h3
-                                        className={`${styles["h3"]} ${styles["card-title"]}`}
+                        {results.map((result) => (
+                            <li className={`${styles["scrollbar-item"]}`}>
+                                <div className={`${styles["work-card"]}`}>
+                                    <figure
+                                        className={`${styles["card-banner"]} ${styles["img-holder"]}`}
+                                        style={{
+                                            width: "350px",
+                                            height: "406px",
+                                        }}
                                     >
-                                        JUMLAH BOOKING 234
-                                    </h3>
+                                        <img
+                                            src={
+                                                result.foto
+                                                    ? "/storage/" + result.foto
+                                                    : "/images/user.jpeg"
+                                            }
+                                            width="350"
+                                            height="406"
+                                            loading="lazy"
+                                            alt="Engine Repair"
+                                            className={`${styles["img-cover"]}`}
+                                        />
+                                    </figure>
 
-                                    <a
-                                        href="#"
-                                        className={`${styles["card-btn"]}`}
+                                    <div
+                                        className={`${styles["card-content"]}`}
                                     >
-                                        <span
-                                            className={`${styles["next-icon"]}`}
+                                        <p
+                                            className={`${styles["card-subtitle"]}`}
                                         >
-                                            <box-icon
-                                                name="right-arrow-alt"
-                                                color="white"
-                                            ></box-icon>
-                                        </span>
-                                    </a>
-                                </div>
-                            </div>
-                        </li>
+                                            Jumlah Booking{" "}
+                                            {result.total_booking}
+                                        </p>
 
-                        
+                                        <h3
+                                            className={`${styles["h3"]} ${styles["card-title"]}`}
+                                        >
+                                            {result.nama}
+                                        </h3>
+
+                                        <a
+                                            href="#"
+                                            className={`${styles["card-btn"]}`}
+                                        >
+                                            <span
+                                                className={`${styles["next-icon"]}`}
+                                            >
+                                                {result.rank}
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </section>
